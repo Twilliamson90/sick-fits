68a652612db50978bd376d95fc191237ecd86212
Travis Williamson <Twilliamson90@gmail.com>
2021-03-27 21:32:28 -0400

    Add initial pages

---
+++ frontend/components/Page.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <Header />
+      <p>Page component</p>
+      <h3>{cool}</h3>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.node),
+};
---
+++ frontend/pages/products.js
@@ -0,0 +1,3 @@
+export default function ProductsPage() {
+  return <p>Hello products page!</p>;
+}
